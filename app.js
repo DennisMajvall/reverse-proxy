534b75e32b2973d654bc7ff8f312e36991a9073b
DennisMajvall <dennis@majvall.se>
2017-11-27 10:47:52 +0100

    changed routes to json

--- app.js
+++ app.js
@@ -1,43 +1,20 @@
 process.chdir(__dirname);
-const http = require('http');
-const https = require('https');
-const httpProxy = require('http-proxy');
-const tls = require('tls');
-const fs = require('fs');
-const path = require('path');
-const exec = require('child_process').exec;
+const http = require('http'),
+  https = require('https'),
+  httpProxy = require('http-proxy'),
+  tls = require('tls'),
+  fs = require('fs'),
+  path = require('path'),
+  exec = require('child_process').exec;
 
 const proxy = httpProxy.createProxyServer({});
-
-const ports = {
-  ez: 3000,
-  test: 3001,
-  test2: 4000,
-  test3: 4000
-}
+proxy.on('error', function(e) { console.log('___Proxy error___', e); });
 
 const certBotPort = 5000;
-const certPath = '/etc/letsencrypt/live'
+const certPath = '/etc/letsencrypt/live';
 
+const routes = require('routes.json');
 let certs = readCerts();
-
-proxy.on('error', function(e) {
-  console.log('___Proxy error___', e);
-});
-
-function setResponseHeaders(res){
-  const oldWriteHead = res.writeHead.bind(res);
-  res.writeHead = (statusCode, headers)=>{
-    res.setHeader('x-powered-by', 'majvall');
-    res.setHeader('strict-transport-security','max-age=31536000; includeSubDomains; preload');
-    res.setHeader('x-frame-options','SAMEORIGIN');
-    res.setHeader('x-xss-protection', '1');
-    res.setHeader('x-content-type-options','nosniff');
-    // Lägg bara till unsafe-inline och unsafe-eval om ni behöver et
-    res.setHeader('content-security-policy',"default-src * 'unsafe-inline' 'unsafe-eval'");
-    oldWriteHead(statusCode, headers);
-  };
-}
 
 https.createServer({
   SNICallback: (domain, cb) => cb(
@@ -49,31 +26,41 @@ https.createServer({
 }, (req, res)=>{
   setResponseHeaders(res);
 
-  const host = req.headers.host; // test.majvall.se/index
-  const domains = host.split('.'); // [ 'test', 'majvall', 'se' ]
+  const host = req.headers.host; // test.majvall.se
+  let url = req.url + (url.substr(-1) != '/' ? '/' : '')
 
-  if (domains[0] == 'www') {
-    let url = 'https://' + domains.slice(1).join('.') + req.url;
-    res.writeHead(301, {'Location': url});
-    res.end();
-    return;
+  let port = routes[host];
+  if (!port) {
+    for (let r in routes) {
+      let val = routes[r];
+      r.includes('/') && (r += req.url + (url.substr(-1) != '/' ? '/' : ''));
+
+      if (r == host) { port = val; }
+      else if (route.indexOf(host + url) == 0){
+        port = val;
+      }
+    }
   }
-
-  const topDomain = domains.pop(); // se
-  const domain = domains.pop(); // majvall
-  const subdomain = domains.join('.'); // test
-
-  const pathname = req.url; // index
-
-  let port = ports[subdomain];
 
   if (!port) {
+
     res.statusCode = 404;
-    res.end('Wrong subdomain. ' + subdomain);
-    return;
+    res.end('Wrong subdomain: ' + subdomain);
+
+  } else if (typeof port == 'number'){
+
+    proxy.web(req, res, { target: 'http://127.0.0.1:' + port });
+
+  } else if (port.redirect){
+
+    let url = 'https://' + port.redirect + req.url;
+    res.writeHead(301, {'Location': url});
+    res.end();
+
+  } else {
+    res.send('Error in routing');
   }
 
-  proxy.web(req, res, { target: 'http://127.0.0.1:' + port });
 }).listen(443, ()=>{
   console.log('Proxy listening on port 443');
 });
@@ -90,6 +77,20 @@ http.createServer((req, res)=>{
 }).listen(80, ()=>{
   console.log('Proxy listening on port 80');
 });
+
+function setResponseHeaders(res){
+  const oldWriteHead = res.writeHead.bind(res);
+  res.writeHead = (statusCode, headers)=>{
+    res.setHeader('x-powered-by', 'majvall');
+    res.setHeader('strict-transport-security','max-age=31536000; includeSubDomains; preload');
+    res.setHeader('x-frame-options','SAMEORIGIN');
+    res.setHeader('x-xss-protection', '1');
+    res.setHeader('x-content-type-options','nosniff');
+    // Lägg bara till unsafe-inline och unsafe-eval om ni behöver et
+    res.setHeader('content-security-policy',"default-src * 'unsafe-inline' 'unsafe-eval'");
+    oldWriteHead(statusCode, headers);
+  };
+}
 
 
 function readCerts() {
