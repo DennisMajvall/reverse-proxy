17055cf9b9ad2926cbf28e35bf2469415def87a4
DennisMajvall <dennis.majvall.sp13@thegameassembly.com>
2018-01-26 01:03:03 +0100

    enabled https

--- app.js
+++ app.js
@@ -12,50 +12,47 @@ const http = require('http'),
 const proxy = httpProxy.createProxyServer({});
 proxy.on('error', function(e) { console.log('___Proxy error___', e); });
 
-const httpPort = 80; // (80 forward-rule)
-const httpsPort = 443; // (443 forward-rule)
 const certBotPort = 5000;
 const certPath = '/etc/letsencrypt/live';
-// let certs = readCerts();
+let certs = readCerts();
 
-// https.createServer({
-//   SNICallback: (domain, cb) => cb(
-//     (certs[domain] ? null : new Error("No such cert")),
-//     (certs[domain] ? certs[domain].secureContext : null)
-//   ),
-//   key: certs['test.majvall.se'].key,
-//   cert: certs['test.majvall.se'].cert
-// }, (req, res)=>{
-//   setResponseHeaders(res);
+https.createServer({
+  SNICallback: (domain, cb) => cb(
+    (certs[domain] ? null : new Error("No such cert")),
+    (certs[domain] ? certs[domain].secureContext : null)
+  ),
+  key: certs['magnets.majvall.com'].key,
+  cert: certs['magnets.majvall.com'].cert
+}, (req, res)=>{
+  setResponseHeaders(res);
 
-//   let portToUse = getRouteFromJSON(req);
+  let portToUse = getRouteFromJSON(req);
 
-//   if (!portToUse) {
-//     res.statusCode = 404;
-//     res.end('Wrong subdomain: ' + req.headers.host);
-//   }
-//   else if (typeof portToUse == 'string'){
-//     // proxy.web(req,res,{target: {host:'ip.addr.here',port:80}});
-//     proxy.web(req, res, { target: portToUse });
-//   }
-//   else if (typeof portToUse == 'number'){
-//     proxy.web(req, res, { target: 'http://127.0.0.1:' + portToUse });
-//   }
-//   else if (portToUse.redirect){
-//     let url = 'https://' + portToUse.redirect + req.url;
-//     res.writeHead(301, {'Location': url});
-//     res.end();
-//   }
-//   else {
-//     res.send('Error in routing');
-//   }
+  if (!portToUse) {
+    res.statusCode = 404;
+    res.end('Wrong subdomain: ' + req.headers.host);
+  }
+  else if (typeof portToUse == 'string'){
+    // proxy.web(req,res,{target: {host:'ip.addr.here',port:80}});
+    proxy.web(req, res, { target: portToUse });
+  }
+  else if (typeof portToUse == 'number'){
+    proxy.web(req, res, { target: 'http://127.0.0.1:' + portToUse });
+  }
+  else if (portToUse.redirect){
+    let url = 'https://' + portToUse.redirect + req.url;
+    res.writeHead(301, {'Location': url});
+    res.end();
+  }
+  else {
+    res.send('Error in routing');
+  }
 
-// }).listen(httpsPort, ()=>{
-  // console.log(`Proxy listening on port 443 (${httpsPort})`);
-// });
+}).listen(443, ()=>{
+  console.log(`Proxy listening on port 443`);
+});
 
 http.createServer((req, res)=>{
-  console.log('request gotten', req.headers.host, req.url);
   if (req.url.indexOf('/.well-known') == 0){
     proxy.web(req, res, { target: 'http://127.0.0.1:' + certBotPort });
     return;
@@ -64,8 +61,8 @@ http.createServer((req, res)=>{
   const url = 'https://' + req.headers.host + req.url;
   res.writeHead(301, {'Location': url});
   res.end();
-}).listen(httpPort, ()=>{
-  console.log(`Proxy listening on port 80 (${httpPort})`);
+}).listen(80, ()=>{
+  console.log(`Proxy listening on port 80`);
 });
 
 function getRouteFromJSON(req){
@@ -101,7 +98,7 @@ function setResponseHeaders(res){
 
 (()=>{
   const express = require('express');
-  express().use(express.static('/var/www/html')).listen(certBotPort, ()=>{ console.log('certbot is listening on:', certBotPort); });
+  express().use(express.static('/var/www/html')).listen(certBotPort);
 })();
 
 function readCerts() {
@@ -131,11 +128,7 @@ function renewCerts(){
 
 // renewCerts();
 // setInterval(renewCerts, 24*60*60*1000);
-// certbot certonly --webroot -w /var/www/html -d test.majvall.se
+// certbot certonly --webroot -w /var/www/html -d magnets.majvall.com
 
 // Error: listen EACCES 0.0.0.0:80 (non-root may not use ports <= 1024)
-// Solution:
-// sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 10080
-// sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 443 -j REDIRECT --to-port 10443
-// also add these to: /etc/rc.local (no need for sudo while inside of that file)
-// to delete a rule change -A to -D
+// Solution: http://pm2.keymetrics.io/docs/usage/specifics/#listening-on-port-80-w-o-root
